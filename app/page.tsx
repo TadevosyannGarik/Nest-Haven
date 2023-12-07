03c29fdd49d6f582125e87015289a423bddef289
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-07 17:50:50 +0400

    Displaying listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 
-export default function Home() {
+export default async function Home() {
+    const listings = await getListings()
+
+    if(listings.length === 0) {
+        return (
+            <EmptyState showReset />
+        )
+    }
+    
     return (
-        <div className=" text-red-400 text-2xl">
-            Hello Nest Haven
-        </div>
+        <Container>
+            <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                {listings.map((listing: any) => {
+                    return (
+                        <div>
+                            {listing.title  }
+                        </div>
+                    )
+                })}
+            </div>
+        </Container>
     )
 }
