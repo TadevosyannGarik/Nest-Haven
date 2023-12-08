1309d1286a67178b413c4d1d3806a0559d337545
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-08 12:16:35 +0400

    Individual Listing View

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
     const listings = await getListings()
-    const currentUser = getCurrentUser()
+    const currentUser = await getCurrentUser()
 
     if(listings.length === 0) {
         return (
@@ -18,7 +18,7 @@ export default async function Home() {
     return (
         <Container>
             <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                {listings.map((listing: any) => {
+                {listings.map((listing) => {
                     return (
                         <div>
                             <ListingCard
