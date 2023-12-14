bfeeeaefaaef3ad6811eae373c8a4d3246830273
TadevosyannGarik <tadevosyan12@gmail.com>
2023-12-14 15:46:31 +0400

    Fix bag for vercel 2

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-    const listings = await getListings(searchParams)
-    const currentUser = await getCurrentUser()
+    const listings = await getListings(searchParams);
+    const currentUser = await getCurrentUser();
 
     if(listings.length === 0) {
         return (
@@ -21,18 +21,14 @@ const Home = async ({ searchParams }: HomeProps) => {
     
     return (
         <Container>
-            <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                {listings.map((listing) => {
-                    return (
-                        <div>
-                            <ListingCard
-                                currentUser={currentUser} 
-                                key={listing.id}
-                                data={listing}
-                            />
-                        </div>
-                    )
-                })}
+            <div className=" pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                {listings.map((listing: any) => (
+                    <ListingCard
+                        currentUser={currentUser}
+                        key={listing.id}
+                        data={listing}
+                    />
+                ))}
             </div>
         </Container>
     )
